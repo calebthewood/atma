8222f6d79bc41dd0458c90be9e03636a8fdcefc6
Caleb Wood <calebw787@gmail.com>
2025-01-27 18:07:14 -1000

    wip subscribe flo

---
+++ actions/misc.ts
@@ -0,0 +1,15 @@
+export async function floDeskSubscribe({ email }: { email: string }) {
+  /*
+  url:
+    https://api.flodesk.com/v1/subscribers
+
+  payload:
+    "email": "string",
+    "source": "manual",
+
+
+
+  */
+
+  // const res = await fetch();
+}
